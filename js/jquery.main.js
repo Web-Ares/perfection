334562da4b0edc3762e16e024a3a140a404691b3
iutin <evgeniy.iutin@gmail.com>
2016-03-02 09:54:11 +0200

    add semibold

--- js/jquery.main.js
+++ js/jquery.main.js
@@ -18,71 +18,55 @@ var Screen = function (obj) {
 
     //private methods
     isUp = function (curentTopPosition) {
+        var isFind = false;
         for (var i = $(_item).length - 1; i >= 0; i--) {
             var itemTopPosition = $(_item[i]).offset().top;
 
-            if (curentTopPosition > itemTopPosition) {
+            if (curentTopPosition > itemTopPosition && !isFind) {
                 _obj.getNiceScroll(0).doScrollTo(itemTopPosition, 300);
-                setTimeout(function () {
-                    _item.mousewheel(function(e){
-                        _func(e);
-                    });
-                }, 1000);
+                isFind = true;
                 return false;
             }
-
         }
-        return false;
+        return isFind;
     };
     isDown = function (curentTopPosition) {
+        var isFind = false;
         _item.each(function () {
             var itemTopPosition = $(this).offset().top;
-            if (curentTopPosition < itemTopPosition) {
+            if (curentTopPosition < itemTopPosition && !isFind) {
                 _obj.getNiceScroll(0).doScrollTo(itemTopPosition, 300);
-                setTimeout(function () {
-                    _item.mousewheel(function(e){
-                        _func(e);
-                    });
-                }, 1000);
+                isFind = true;
                 return false;
             }
 
         });
-
-        return false;
+        return isFind;
     };
     _func = function (event) {
         var curentTopPosition = $('.site').getNiceScroll(0).getScrollTop();
-        _item.unmousewheel();
+        _item.unmousewheel(_func);
 
         console.log($('.site').getNiceScroll(0).getScrollTop());
-        //$('.site').getNiceScroll(0).unbindAll();
+        $('.site').getNiceScroll(0).unbindAll();
 
         if (event.deltaY > 0) {//вверх
-            if (!isUp(curentTopPosition)) {
+            isUp(curentTopPosition)
                 setTimeout(function () {
-                    _item.mousewheel(function(e){
-                        _func(e);
-                    });
-                }, 1000);
-            }
-        } else {// вниз
-            if (!isDown(curentTopPosition)) {
-                setTimeout(function () {
-                    _item.mousewheel(function(e){
-                        _func(e);
-                    });
-                }, 1000);
-            }
-        }
+                    _item.mousewheel(_func);
+                }, 500);
 
+        } else {// вниз
+            isDown(curentTopPosition);
+                setTimeout(function () {
+                    _item.mousewheel(_func);
+                }, 500);
+
+        }
 
     };
     var _onEvents = function () {
-
-            _item.mousewheel(function(e){
-                _func(e);
-            });
+            _item.mousewheel(_func);
         },
         _initContentScroll = function () {
             _obj.niceScroll({

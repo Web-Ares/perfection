c156d76ed6673b4239a6af11749e571f403856e0
petruninevgeniy <petruninevgeniy@gmial.com>
2016-03-03 11:35:37 +0200

    sharpest

--- js/jquery.main.js
+++ js/jquery.main.js
@@ -5,6 +5,12 @@ $(function () {
         new Screen($(this));
     });
 
+    $.each( $( '.formats__slider' ), function() {
+
+        new SliderFormats ( $( this ) );
+
+    } );
+
     var Menu = function () {
         var _self = this, _btn = $('.site__header__menu-drop'),
             _dropdown = $('.drop-menu'),
@@ -216,6 +222,181 @@ var Screen = function (obj) {
     //public properties
 
     //public methods
+    _init();
+};
+
+var SliderFormats = function( obj ) {
+
+    //private properties
+    var _self = this,
+        _obj = obj,
+        _objInner = _obj.find( '>div'),
+        _items = _objInner.find( '.formats__slider-item'),
+        _textItems = _objInner.find( '.formats__slider-text>div'),
+        _timer,
+        _distance = 0 ,
+        _window = $( window );
+
+    //private methods
+    var _addEvents = function() {
+
+            _items.on( {
+                'mouseenter': function() {
+
+                    if( _window.width() >= 992 ) {
+
+                        _changeText( $( this ) );
+
+                    }
+                }
+            } );
+
+            _items.on( {
+                'click': function() {
+
+                    if( _window.width() < 992 ) {
+
+                        _changeText( $( this ) );
+
+                    }
+
+                    return false
+
+                }
+            } );
+
+            _obj.on( {
+
+                'mouseenter': function(){
+
+                    clearInterval( _timer );
+
+                },
+
+                'mouseleave': function(){
+
+                    _changeNext();
+
+                }
+
+            } );
+
+            _window.on( {
+                'resize': function() {
+
+                    _positionItems();
+
+                }
+            } )
+
+        },
+        _changeText = function( item ) {
+
+            var itemIndex = item.index(),
+                textBlock = _textItems.eq( itemIndex );
+
+            _items.removeClass( 'active' );
+
+            item.addClass( 'active' );
+
+            _textItems.removeClass( 'visible' );
+
+            textBlock.addClass( 'visible' );
+
+            clearInterval( _timer );
+
+        },
+        _positionItems = function() {
+
+            if( _window.width() < 768 ) {
+
+                _distance = 0;
+
+            } else if( _window.width() >= 768 && _window.width() < 1200 ) {
+
+                _distance = 70;
+
+            } else {
+
+                _distance = 54;
+
+            }
+
+            var radius = (_objInner.width() + _distance) / 2 + 'px',
+                start = -90,
+                numberOfElements = _items.length,
+                slice = 360 / numberOfElements;
+
+            _items.each( function( i ) {
+
+                var curItem = $( this ),
+                    rotate = slice * i + start,
+                    rotateReverse = rotate * -1;
+
+                curItem.css( {
+                    '-webkit-transform': 'rotate(' + rotate + 'deg) translate(' + radius + ') rotate(' + rotateReverse + 'deg)',
+                    'transform': 'rotate(' + rotate + 'deg) translate(' + radius + ') rotate(' + rotateReverse + 'deg)'
+                } );
+
+            } );
+        },
+        _changeNext = function() {
+
+            _timer = setInterval( function() {
+
+                var activeItem = _items.filter( '.active'),
+                    nextItem = activeItem.next(),
+                    nextItemIndex = nextItem.index(),
+                    currentTextBlock = _textItems.eq( nextItemIndex );
+
+                if( nextItem.index() == - 1 ) {
+
+                    nextItem = _items.eq( 0 );
+                    currentTextBlock = _textItems.eq( 0 );
+
+                }
+
+                _items.removeClass( 'active' );
+
+                _textItems.removeClass( 'visible' );
+
+                nextItem.addClass( 'active' );
+
+                currentTextBlock.addClass( 'visible' );
+
+            }, 5000 );
+
+        },
+        _startView = function() {
+
+
+            setTimeout( function () {
+
+                clearInterval( _timer );
+
+                _items.eq(0).addClass( 'active' );
+
+                var activeItem = _items.filter( '.active'),
+                    activeItemIndex = activeItem.index(),
+                    currentTextBlock = _textItems.eq( activeItemIndex );
+
+                currentTextBlock.addClass( 'visible' );
+
+            },3000 );
+
+
+            _changeNext();
+
+        },
+        _init = function() {
+
+            _obj[ 0 ].obj = _self;
+            _startView();
+            _positionItems();
+            _addEvents();
+
+        };
+
     _init();
 };
 

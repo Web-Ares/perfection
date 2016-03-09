d92926d5b44479e88fc3227b02b652a0346e4e60
petruninevgeniy <petruninevgeniy@gmial.com>
2016-03-09 11:06:11 +0200

    promo

--- js/jquery.main.js
+++ js/jquery.main.js
@@ -45,14 +45,16 @@ var Menu = function (obj) {
                 scroll: function() {
                     _siteSections.each( function() {
                         var siteSectionsTop = $( this ).offset().top,
-                            siteSectionsHeight = $( this ).offsetHeight();
+                            siteSectionsHeight = $( this ).height();
 
                         if( siteSectionsTop <= $( window ).scrollTop() ) {
                             _header.removeClass( 'white' );
                             _header.addClass( $( this ).data( 'header-color' ) );
                         }
 
-                        if( ( siteSectionsTop <= $( window ).scrollTop() + 80 ) && ( siteSectionsTop + siteSectionsHeight >= $( window ).scrollTop() + 80 ) ) {
+                        console.log (siteSectionsHeight)
+
+                        if( ( siteSectionsTop <= $( window ).scrollTop() + 80 ) && ( siteSectionsTop + siteSectionsHeight >= $( window ).scrollTop() - 80 ) ) {
                             _siteSections.removeClass ( 'active' );
                             $( this ).addClass( 'active' );
                         }
